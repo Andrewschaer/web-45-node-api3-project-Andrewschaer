17120e586c288a2814d51b9db94a142163735fde
Andrewschaer <andrewschaer@github.com>
2021-09-02 03:57:03 -0400

    first two endpoints with middleware added w/ passing tests

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,23 @@
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(req.method, req.url, Date.now())
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  const { id } = req.params;
+  Users.getById(id)
+    .then(possibleUser => {
+      if (possibleUser) {
+        req.user = possibleUser;
+        next();
+      } else {
+        res.status(404).json({ message: 'user not found'});
+      }
+    })
+    .catch(next);
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +29,4 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost };
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,17 +1,36 @@
 const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
+const Users = require('./users-model');
+const Posts = require('../posts/posts-model');
 // The middleware functions also need to be required
+const { validateUserId, validateUser, validatePost } = require('../middleware/middleware');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+    .then(usersList => {
+      res.status(200).json(usersList);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({message: 'There was an error retrieving the users'})
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  Users.getById(req.params.id)
+    .then(thing => {
+      res.status(200).json(thing);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({message: 'There was an error retrieving the user'})
+    });
 });
 
 router.post('/', (req, res) => {
@@ -42,3 +61,4 @@ router.post('/:id/posts', (req, res) => {
 });
 
 // do not forget to export the router
+module.exports = router;
